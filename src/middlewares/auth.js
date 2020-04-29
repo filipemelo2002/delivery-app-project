59183cce212d1f5aa1c82d41489914b2024344b0
Filipe Melo <filipemelo032@gmail.com>
2020-04-29 12:01:11 -0300

    first commit before runing tests

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,32 @@
 const Admin = require('../models/Admin')
 const jwt = require('jsonwebtoken')
 module.exports = async (req, res, next) => {
-  const { access_token } = req.headers
+  const { authorization } = req.headers
 
-  if (!access_token) return res.status(401).json({ message: 'access not permited' })
+  if (!authorization) return res.status(401).json({ message: 'access not permited' })
 
-  try {
-    const { _id } = await jwt.verify(access_token, 'rfBDemjiLSZyEjC')
-    const admin = await Admin.findOne({
-      _id
-    })
-    if (admin) {
-      req.refresh_token = await jwt.sign({ _id: admin._id }, 'rfBDemjiLSZyEjC', { expiresIn: '1h' })
-      next()
+  const parts = authorization.split(' ')
+
+  if (parts.length === 2) {
+    const [scheme, token] = parts
+
+    if (!/^Bearer$/i.test(scheme)) return res.status(401).json({ message: 'invalid token' })
+
+    try {
+      const { _id } = await jwt.verify(token, 'rfBDemjiLSZyEjC')
+
+      const admin = await Admin.findOne({
+        _id
+      })
+      if (admin) {
+        const refresh_token = await jwt.sign({ _id: admin._id }, 'rfBDemjiLSZyEjC', { expiresIn: '1h' })
+        res.set('Refresh-Token', refresh_token)
+        next()
+      }
+    } catch (err) {
+      res.status(401).json({ message: 'token expired' })
     }
-  } catch (err) {
-    res.status(401).json({ message: 'access not permited' })
+  } else {
+    return res.status(401).json({ message: 'token error' })
   }
 }
