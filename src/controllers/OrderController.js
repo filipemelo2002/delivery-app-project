6f1dd25050b023692d1b685bd4cb0269ccd06eb8
Filipe Melo <filipemelo032@gmail.com>
2020-04-29 15:50:04 -0300

    Sanitized order id setter

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,9 +1,14 @@
 const crypto = require('crypto')
 const Order = require('../models/Order')
 module.exports = {
+  async index (req, res) {
+    const order = await Order.find()
+
+    return res.json(order)
+  },
   async create (req, res) {
     const data = req.body
-    const order_id = crypto.crypto.randomBytes(3).toString('HEX')
+    const order_id = crypto.randomBytes(3).toString('HEX')
 
     data.order_id = order_id
 
